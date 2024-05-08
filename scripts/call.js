50220e8ffed263f7f6c4bb65be7fdb7dd09e68a1
Khomenko Vladyslav <vld.khmnko@gmail.com>
2024-05-08 23:04:58 +0300

    change http on https

--- scripts/call.js
+++ scripts/call.js
@@ -1,7 +1,7 @@
 
 async function fetchAndParse(city) {
     const API_KEY = 'fe40dfed2d6d488c8bc182952240805'
-    let url = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=10&aqi=yes&alerts=no`;
+    let url = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=10&aqi=yes&alerts=no`;
 
     try {
         const response = await fetch(url);
